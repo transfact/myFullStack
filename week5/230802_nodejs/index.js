9fc17a86b36cbfb90d51761893b4da63cb5153e3
transfact <transpaction@naver.com>
2023-08-02 19:13:09 +0900

    pratice: login using ajax

--- week5/230802_nodejs/index.js
+++ week5/230802_nodejs/index.js
@@ -9,9 +9,47 @@ app.use(express.json())
 app.set("view engine","ejs");
 app.set('views','./views');
 
+
+
 app.get('/',(req,res)=>{
-    res.render('index')
+    // res.render('index')
+    res.render('login')
 })
+
+
+idpwObject= {}
+for(let i=1; i<=100; i++){
+    idpwObject[`gijin${i}`] = 1234;
+}
+
+// pratice1 
+
+app.get('/ajaxSignIn',(req,res)=>{
+    //get은 query
+    console.log('back_ajax_signin',req.query)
+    //원래라면 중간 과정이 있다 그래서 임의로 만들어주었다
+    res.send(req.query)
+})
+
+app.post('/ajaxLogin',(req,res)=>{
+    //get은 query
+    console.log('back_ajaxLogin',req.body)
+    let id=req.body["id"]
+    let pw=req.body['pw']
+    
+    if(id in idpwObject){
+        if(idpwObject[id]==pw){
+            //성공
+            res.send({"try" : "success"})
+        }else{
+            res.send({"try" : "pw_err"})
+        }
+    }else{
+        res.send({"try" : "id_err"})
+    }
+})
+
+
 
 app.get('/axios',(req,res)=>{
     //get은 query

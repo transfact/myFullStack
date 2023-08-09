ef9a67b217b6c938fbb4471accc856ec072450c9
transfact <transpaction@naver.com>
2023-08-09 15:08:51 +0900

    스태틱 static css 적용

--- week6/230808_mysql_nodejs/index.js
+++ week6/230808_mysql_nodejs/index.js
@@ -7,6 +7,7 @@ app.set("views","./views")
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use("/static",express.static(__dirname+"/static"))
 
 //라우터 가져오기
 // index를 생략가능하다(코드에서는 생략안할것임)
@@ -14,6 +15,10 @@ const router =require("./routes/rUser")
 //const visitorRouter = require("/visitor")
 //app.use("/router/visitor",visitorRouter)
 app.use(router)
+
+app.get("*",(req,res)=>{
+    res.render("404")
+})
 
 app.listen(PORT, ()=>{
     console.log(`http://localhost:${PORT}/user`);

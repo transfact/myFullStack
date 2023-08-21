af8e39ffaa91e9d48b21343545d4483ff023d0bb
transfact <transpaction@naver.com>
2023-08-21 15:40:30 +0900

    bcrypt

--- week7/230818_pw/controller/cUser.js
+++ week7/230818_pw/controller/cUser.js
@@ -10,22 +10,43 @@ exports.toLogIn = (req,res)=>{
     res.render('login');
 }
 //로그인기능
+
+//로그인
+exports.LogIn2 = async (req, res) => {
+    const { id, pw } = req.body;
+    const result = await User.findOne({
+        where: { userid :id },
+    });
+    if (!result) {
+        res.json({ result: false, message: '사용자가 존재하지 않습니다' });
+    }
+    const compare = comparePassword(pw, result.pw);
+    if (compare) {
+        console.log("login")
+        res.json({ result: true });
+    } else {
+        console.log("login fails")
+        res.json({ result: false, message: '비밀번호가 일치하지 않습니다' });
+    }
+};
 exports.LogIn = async (req,res)=>{
     const {id,pw}= req.body
-    console.log(id,pw)
-    const password = bcrypt.hashSync(pw,bc_salt)
-    console.log("pw:" , password)
     let result = await User.findOne({
         where : {
-            userid : id,
-            pw :password
+            userid : id
         }
     })
-    
-    console.log(result)
-    if(result){
-        res.send({code:1});
+    if(!result){
+        console.log("login fails")
+        res.json({ result: false, message: '아이디가 일치하지 않습니다' });
+        return;
+    }
+    const compare = comparePassword(pw, result.pw);
+    if(compare){
+        console.log("login")
+        res.send({code:1,result});
     }else{
+        console.log("login fails")
         res.send({code:0});
     }
 
@@ -39,6 +60,7 @@ exports.toSignUp = (req,res)=>{
 //회원가입기능
 exports.SignUp = async (req,res)=>{
     const {id,pw,name}= req.body
+    
     console.log(id,pw,name)
     
     const password = bcrypt.hashSync(pw,bc_salt)
@@ -51,7 +73,7 @@ exports.SignUp = async (req,res)=>{
     console.log(result)
 
     if(result.id!=undefined){
-        res.send({code:1});
+        res.send({code:1,result});
     }else{
         res.send({code:0});
     }
@@ -62,3 +84,6 @@ const bcryptPassword = (password,bc_salt) =>{
 }
 
 //비교
+const comparePassword = (password, dbPassword) => {
+    return bcrypt.compareSync(password, dbPassword);
+};

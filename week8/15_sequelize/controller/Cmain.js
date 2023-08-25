1b050f09e772b6d6a114e88f963c3bb500dc1294
transfact <transpaction@naver.com>
2023-08-25 15:53:11 +0900

    cookies

--- week8/15_sequelize/controller/Cmain.js
+++ week8/15_sequelize/controller/Cmain.js
@@ -4,19 +4,56 @@ const { User } = require('../models');
 const bcrypt = require("bcrypt")
 const salt = 9
 
+//쿠키 설정하기
+const cookieConfig = {
+    httpOnly : true,
+    maxAge : 60 * 1000 ,//10분 ,
+}
 
+//cookie옵션개체
+const cookieConfig2  = {
+    //httpOnly 웹서버를 통해서만 쿠키에 접근 가능 (document.cookie 불가)
+    //maxAge :쿠키의 수명 (ms단위)
+    //expires : 만료날짜 GMT 시간 설정
+    //path 해당 디렉토리와 하위 디렉토리에서만 경로 활성화, 웹브라우저는 해당하는 쿠키만 웹서버에 전송
+    //보내고 싶은 쿠키만 보낼 수 있다. default : /
+    //domain 쿠키가 전송될 도메인을 특정할 수 있따.
+    //secure : 웹브라우저와 웹서버가 https 일경우면 가능
+    //signed : 쿠키의 암호화결정 (req.signedCookies 객체에 들어있다고 함)
+    httpOnly : true,
+    maxAge : 60 * 1000, //10분
+    signed : true,
+}
 ////////////////////////////////
 //GET
 //메인페이지
 const main = (req, res) => {
+    //쿠키 사용
+    //
+    console.log("cookie : ",req.cookies)
     res.render('index');
 };
 //회원가입페이지
 const signup = (req, res) => {
+    if(req.session.userInfo){
+        res.redirect("/")
+    }
+    //쿠키 생성
+    // 쿠키 이름, 쿠키값, 옵션객체
+    res.cookie('signUpCookie',"signUp",cookieConfig)
     res.render('signup');
 };
 //로그인페이지
 const signin = (req, res) => {
+    try{
+        console.log(req.session.userInfo)
+    }catch{
+        console.log("pass")
+    }
+    if(req.session.userInfo){
+
+        res.redirect(`/profile/${req.session.userInfo.id}`)
+    }
     res.render('signin');
 };
 //회원정보 조회 페이지
@@ -40,13 +77,17 @@ const buy = (req, res) => {};
 //POST
 //회원가입
 const post_signup = (req, res) => {
+
+
+
     // model.db_signup(req.body, () => {
     //     res.json({ result: true });
     // });
     const { userid, name, pw } = req.body;
+    
+    //실습과제 - 비밀번호 암호화하여 저장
     const newpw = bcrypt.hashSync(pw,salt)
     //create 데이터 생성
-    //실습과제 - 비밀번호 암호화하여 저장
     
     User.create({ userid, name, pw : newpw }).then(() => {
         res.json({ result: true });
@@ -65,6 +106,8 @@ const post_signin =async (req, res) => {
     const result =  bcrypt.compareSync(pw,res1.pw)
     if (result) {
         console.log("성공", res1)
+        //세션 생성
+        req.session.userInfo=res1
         res.json({ result: true, data: res1 });
     } else {
         console.log("실패", pw,res1.pw)
--- week8/15_sequelize/index.js
+++ week8/15_sequelize/index.js
@@ -3,10 +3,29 @@ const app = express();
 const PORT = 8000;
 const db = require('./models');
 
+//세션 선언
+const session = require("express-session")
+
+//쿠키는 여기서 선언해라.
+const cookieParser = require("cookie-parser")
+
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cookieParser())
+app.use(session({
+    secret : "mySession", //서명
+    secure : false, // true면 https 에서만
+    resave : false, // true면 수정사항이 생기지 않아도 매 요청마다 세션을 저장한다 (false 권장)
+    saveUninitialized :true, //세션에 저장할 내역이 없어도 처음부터 세션을 생성할 시 생성할지 설정 (보통 TRUE) 
+    //쿠키
+    cookie : {
+        httpOnly : false,
+        maxAge : 60 * 1000 * 1
+    }, 
+    }
+))
 //router 분리
 const router = require('./routes/main');
 app.use('/', router);

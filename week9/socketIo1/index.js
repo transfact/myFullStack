6a5ca86758a57ff09c354f996842a1fe2858ab03
transfact <transpaction@naver.com>
2023-08-30 17:54:01 +0900

    socketio : kick out

--- week9/socketIo1/index.js
+++ week9/socketIo1/index.js
@@ -22,7 +22,7 @@ app.get("/",(req,res)=>{
 app.get("/chat",(req,res)=>{
     res.render("chat")
 })
-const room_participants = {}
+
 const nicknames={}
 
 io.on("connection",(socket)=>{
@@ -30,12 +30,17 @@ io.on("connection",(socket)=>{
         //채팅방을 생성하는 방법은 join 방아이디 사용 이미 존재하면 들어감
         socket.join(res.name)
         socket.room= res.name;
-        console.log("socketroom",socket.room)
+        console.log("myroom",socket.room)
         //broadcast 나를 제외한 전체사용자에게 메시지 전달
-        socket.broadcast.to(res.name).emit("create",`${socket.id}님이 입장하였습니다.`)
-        const roomInfo = io.sockets.adapter.rooms.get(socket.room)
-        room_participants[socket.room]=roomInfo
-        socket.to(res.name).emit("getId",room_participants,nicknames)
+        socket.myNickname=res.nickname
+        socket.broadcast.to(res.name).emit("create",`${res.nickname}님이 입장하였습니다.`)
+
+        if(!nicknames[socket.room]){
+            nicknames[socket.room]={}
+        }
+        nicknames[socket.room][res.nickname] = socket.id
+        io.to(res.name).emit("getId",nicknames[socket.room])
+
     })
 
     socket.on('message', (res)=>{
@@ -44,14 +49,27 @@ io.on("connection",(socket)=>{
     socket.on('message2', (user,message)=>{
         //아이디 변화 감지시
         if(socket.myNickname == undefined || socket.myNickname!=user){
-            delete nicknames[socket.myNickname]
+            delete nicknames[socket.room][socket.myNickname]
             socket.myNickname=user
-            nicknames[user]=socket.id
-            console.log(nicknames)
+            nicknames[socket.room][user]=socket.id
+            console.log("message2",nicknames)
             //아이디 전부 보내주기
-            socket.to(socket.room).emit('getId',room_participants,nicknames)
+            io.to(socket.room).emit('getId',nicknames[socket.room])
         }
         socket.broadcast.to(socket.room).emit('chat2',{user,message})
+    })
+    //귓속말
+    socket.on('message3', (user,message,toUser)=>{
+        //아이디 변화 감지시
+        if(socket.myNickname == undefined || socket.myNickname!=user){
+            delete nicknames[socket.room][socket.myNickname]
+            socket.myNickname=user
+            nicknames[socket.room][user]=socket.id
+            console.log("message3",nicknames)
+            //아이디 전부 보내주기
+            io.to(socket.room).emit('getId',nicknames[socket.room])
+        }
+        socket.to(toUser).emit('chat3',{user,message})
     })
     socket.on("open_message",(msgObject)=>{
         console.log(msgObject)
@@ -59,12 +77,18 @@ io.on("connection",(socket)=>{
     })
     socket.on("form-message",(msgObject)=>{
         console.log(msgObject)
-        socket.broadcast.emit("backend_message",msgObject)
+        socket.emit("backend_message",msgObject)
     })
     socket.on('leave',()=>{
+        socket.to(socket.room).emit("create",`${socket.myNickname}님이 퇴장하였습니다.`)
         socket.leave(socket.room)
-        const roomInfo = io.sockets.adapter.rooms.get(socket.room)
-        room_participants[socket.room]=roomInfo
+    })
+    socket.on('getOut',(id)=>{
+        console.log("fired")
+        
+        socket.to(nicknames[socket.room][id]).emit('fired')
+        delete nicknames[socket.room][id]
+        io.to(socket.room).emit('getId',nicknames[socket.room])
     })
     // socket.on("assignment1",(msgObject)=>{
     //     console.log(msgObject)
@@ -75,5 +99,5 @@ io.on("connection",(socket)=>{
 
 
 server.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`)
+    console.log(`http://localhost:${PORT}/chat`)
 })

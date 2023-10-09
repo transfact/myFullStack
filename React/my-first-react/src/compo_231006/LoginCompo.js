4746e086025373c925083478bc0082d431304dfb
transfact <transpaction@naver.com>
2023-10-09 16:20:53 +0900

    local

--- React/my-first-react/src/compo_231006/LoginCompo.js
+++ React/my-first-react/src/compo_231006/LoginCompo.js
@@ -1,5 +1,26 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { loginAction } from './store/logInIndex';
+function simulateClick() {
+    var clickEvent = new MouseEvent('click', {
+        bubbles: true,
+        cancelable: true,
+        view: window,
+    });
+    myButton.dispatchEvent(clickEvent);
+}
+function startPress() {
+    if (!isPressing) {
+        isPressing = true;
+        pressInterval = setInterval(simulateClick, 100); // 0.1초마다 클릭 이벤트를 발생시킵니다.
+    }
+}
+
+function stopPress() {
+    if (isPressing) {
+        isPressing = false;
+        clearInterval(pressInterval);
+    }
+}
 
 export default function LoginCompo() {
     const dispatch = useDispatch();

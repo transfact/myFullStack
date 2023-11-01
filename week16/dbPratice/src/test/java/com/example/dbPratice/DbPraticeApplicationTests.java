200ab7dc3c397abd3fb3758e945efebcede87bea
transfact <transpaction@naver.com>
2023-11-01 15:09:15 +0900

    init

---
+++ week16/dbPratice/src/test/java/com/example/dbPratice/DbPraticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbPratice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbPraticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
